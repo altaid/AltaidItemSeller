6e157313073e47c29cdce2e5ec1b5490101ca687
Alexey Astafev <efsneiron@gmail.com>
2017-08-04 12:53:11 +0300

    have fix

--- AltaidItemSeller.lua
+++ AltaidItemSeller.lua
@@ -9,17 +9,17 @@ frame:SetScript("OnEvent", eventHandler)
 
 function seller:sell()
     for bag = 0, 4 do
-    for i = 1, GetContainerNumSlots(bag) do
-        local _, _, _, quality = GetContainerItemInfo(bag, i)
-        local itemLink = GetContainerItemLink(bag, i)
+        for i = 1, GetContainerNumSlots(bag) do
+            local _, _, _, quality = GetContainerItemInfo(bag, i)
+            local itemLink = GetContainerItemLink(bag, i)
 
-        if itemLink then
-            if quality == 0 then
-                UseContainerItem(bag, i)
+            if itemLink then
+                if quality == 0 then
+                    UseContainerItem(bag, i)
+                end
             end
         end
     end
-end
 end
 
 function seller:repair()
